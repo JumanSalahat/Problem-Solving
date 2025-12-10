6c9f41b373a80624c5611cfc6c764dfb1b5a9dcc
JumanSalahat <juman.salahat@gmail.com>
2025-12-10 02:26:17 +0200

    Drinks.cpp
    Little Vasya loves orange juice very much. That's why any food and drink in his kitchen necessarily contains orange juice. There are n drinks in his fridge, the volume fraction of orange juice in the i-th drink equals pi percent.
    
    One day Vasya decided to make himself an orange cocktail. He took equal proportions of each of the n drinks and mixed them. Then he wondered, how much orange juice the cocktail has.
    
    Find the volume fraction of orange juice in the final drink.
    
    Input
    The first input line contains a single integer n (1 ≤ n ≤ 100) — the number of orange-containing drinks in Vasya's fridge. The second line contains n integers pi (0 ≤ pi ≤ 100) — the volume fraction of orange juice in the i-th drink, in percent. The numbers are separated by a space.
    
    Output
    Print the volume fraction in percent of orange juice in Vasya's cocktail. The answer will be considered correct if the absolute or relative error does not exceed 10 ^- 4.
    
    
    Examples :
    Input :
    3
    50 50 100
    Output :
    66.666666666667
    
    Input :
    4
    0 25 50 75
    Output :
    37.500000000000
    
    
    Note :
    Note to the first sample: let's assume that Vasya takes x milliliters of each drink from the fridge. Then the volume of pure juice in the cocktail will equal ( x/2 + x/2 + x = 2x ) milliliters. The total cocktail's volume equals 3·x milliliters, so the volume fraction of the juice in the cocktail equals  ( 2x/3x = 2/3 ) , that is, 66.(6) percent.

--- Codeforces/ PROBLEMLSET/Difficulty : 800-800/Drinks.cpp
+++ Codeforces/ PROBLEMLSET/Difficulty : 800-800/Drinks.cpp
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
-  Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -14,12 +6,12 @@ int main() {
     cin>>n;
     int x=n; // خزّنت قيمة -> n -> في متغيّر جديد ، لإنّي رح أرجع أستخدمها فيما بعد ( للحفاظ على القيمة نفسها)
     double sum=0;
-    // He took equal proportions of each of the n drinks and mixed them -> معلومة بالسؤال مهمّة جداً
-    // أخذ كميات متساوية من كل مشروب وخلطهم مع بعض
+    // He took equal proportions of each of the n drinks and mixed them -> معلومة بالسّؤال مهمّة جداً
+    // أخذ كميّات متساوية من كل مشروب وخلطهم مع بعض
     // يعني من كل مشروب أخد نفس الحجم
     
-    // لهيك فكرة حل السؤال : النسبة النهائية = متوسط النِّسَب 
-    // متوسّط النسب = مجموع النسب ÷ عدد المشروبات
+    // لهيك فكرة حل السّؤال : النّسبة النّهائية = متوسط النِّسَب 
+    // متوسّط النّسب = مجموع النّسب ÷ عدد المشروبات
     while(x--)
     {
         cin>>p;
