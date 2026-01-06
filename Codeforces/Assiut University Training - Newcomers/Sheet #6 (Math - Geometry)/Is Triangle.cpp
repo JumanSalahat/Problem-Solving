bac04be228a50b1302c9c2db864fb327c9e43df9
JumanSalahat <juman.salahat@gmail.com>
2026-01-06 22:27:57 +0200

    Is Triangle
    Given three numbers A, B and C that donate the lengths of the three sides of a triangle. Determine whether this triangle is valid or not and if it is valid print its area.
    
    Input
    Only one line contains three numbers A, B and C (1 ≤ A, B, C ≤ 10^3).
    
    Output
    If the three sides preform a triangle print "Valid" and the area of the triangle in separate lines otherwise, print "Invalid".
    
    Don't print any extra spaces.
    
    
    Examples
    Input :
    7 10 5
    Output :
    Valid
    16.248077
    
    Input :
    3 4 1
    Output :
    Invalid
    
    
    Note
    Your answer will be considered correct if its absolute or relative error does not exceed 10 ^- 4 .

--- Codeforces/Assiut University Training - Newcomers/Sheet #6 (Math - Geometry)/Is Triangle.cpp
+++ Codeforces/Assiut University Training - Newcomers/Sheet #6 (Math - Geometry)/Is Triangle.cpp
@@ -6,8 +6,10 @@ int main()
     cin>>a>>b>>c;
     
     // قانون هيرون
+    // استخدمت هذا القانون لإيجاد مساحة المثلث .. لإني ما بعرف مين القاعدة والارتفاع للمثلث
     double s = (a+b+c ) / 2 ;
     double area = sqrt(s*(s-a)*(s-b)*(s-c)) ;
+    
     if(a+b > c && a+c > b && c+b > a) 
     {
         cout<<"Valid\n"<<area;
