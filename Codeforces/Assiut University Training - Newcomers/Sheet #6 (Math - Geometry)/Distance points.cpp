61f0f0c0f6cf8d0f27f49554033c215bc4ff503c
JumanSalahat <juman.salahat@gmail.com>
2026-01-07 10:34:44 +0200

    Distance points
    Given 2 Cartesian points (X1, Y1) and (X2, Y2). Print the distance between the two points.
    
    Input
    Only one line contains four numbers X1, Y1, X2 and Y2 (1 ≤ X1, Y1, X2, Y2 ≤ 10^9).
    
    Output
    Print the answer required above.
    
    
    Examples
    Input :
    5 3 22 11
    Output :
    18.788294228
    
    Input :
    2 3 1 1
    Output :
    2.236067977
    
    
    Note
    Your answer will be considered correct if its absolute or relative error does not exceed 10^ - 6.

---
+++ Codeforces/Assiut University Training - Newcomers/Sheet #6 (Math - Geometry)/Distance points.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{   
+    long long x1,y1,x2,y2;
+    cin>>x1>>y1>>x2>>y2;
+    long long a = x2-x1;
+    long long b = y2 - y1;
+    long long c = a*a + b*b;
+    double ans = sqrt(c);
+    cout<<fixed << setprecision(6)<<ans;
+    return 0;
+}
