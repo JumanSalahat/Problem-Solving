75f6bc5a005d33639beea3cef73d5dc18b666ad8
JumanSalahat <juman.salahat@gmail.com>
2026-01-10 13:24:45 +0200

    Count Letters
    Given a string S. Determine how many times does each letter occurred in S.
    
    Input
    Only one line contains the string S (1 ≤ |S| ≤ 10^7) where |S| is the length of the string and it consists of only lowercase English letters.
    
    Output
    For each character that appears in S, print a single line that contains the following format: "X : Y" where X is the letter and Y is the number of times that letter X occurred in S.
    
    Note: you must print letters in ascending order.
    
    
    Examples
    Input :
    aaabbc
    Output :
    a : 3
    b : 2
    c : 1
    
    Input :
    regff
    Output :
    e : 1
    f : 2
    g : 1
    r : 1

---
+++ Codeforces/Assiut University Training - Newcomers/Sheet #4 (Strings)/Count Letters.cpp
@@ -0,0 +1,77 @@
+// بعرف حلّي هيك طويل جداً وممكن في طريقة حل مختصرة أو فنكشن مثلاً
+//  ولكن هاي الفكرة إللي إجت ببالي أوّل مرة وتكاسلت أطبّقها وأحل السؤال من قبل
+// الصّبر حلو =)
+
+// Runtime = 156 ms , Memory = 22000 KB
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{   
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    string st;
+    cin>>st;
+    int a=0,b=0,c=0,d=0,e=0,f=0,g=0,h=0,i=0,j=0,k=0,l=0,m=0,n=0,o=0,p=0,q=0,
+    r=0,s=0,t=0,u=0,v=0,w=0,x=0,y=0,z=0;
+    
+    for(int index=0;index<st.size();index++)
+    {
+        if(st[index]=='a') a++;
+        else if(st[index]=='b') b++;
+        else if(st[index]=='c') c++;
+        else if(st[index]=='d') d++;
+        else if(st[index]=='e') e++;
+        else if(st[index]=='f') f++;
+        else if(st[index]=='g') g++;
+        else if(st[index]=='h') h++;
+        else if(st[index]=='i') i++;
+        else if(st[index]=='j') j++;
+        else if(st[index]=='k') k++;
+        else if(st[index]=='l') l++;
+        else if(st[index]=='m') m++;
+        else if(st[index]=='n') n++;
+        else if(st[index]=='o') o++;
+        else if(st[index]=='p') p++;
+        else if(st[index]=='q') q++;
+        else if(st[index]=='r') r++;
+        else if(st[index]=='s') s++;
+        else if(st[index]=='t') t++;
+        else if(st[index]=='u') u++;
+        else if(st[index]=='v') v++;
+        else if(st[index]=='w') w++;
+        else if(st[index]=='x') x++;
+        else if(st[index]=='y') y++;
+        else if(st[index]=='z') z++;
+    }
+    
+    if(a>0) cout<<"a : "<<a<<'\n';
+    if(b>0) cout<<"b : "<<b<<'\n';
+    if(c>0) cout<<"c : "<<c<<'\n';
+    if(d>0) cout<<"d : "<<d<<'\n';
+    if(e>0) cout<<"e : "<<e<<'\n';
+    if(f>0) cout<<"f : "<<f<<'\n';
+    if(g>0) cout<<"g : "<<g<<'\n';
+    if(h>0) cout<<"h : "<<h<<'\n';
+    if(i>0) cout<<"i : "<<i<<'\n';
+    if(j>0) cout<<"j : "<<j<<'\n';
+    if(k>0) cout<<"k : "<<k<<'\n';
+    if(l>0) cout<<"l : "<<l<<'\n';
+    if(m>0) cout<<"m : "<<m<<'\n';
+    if(n>0) cout<<"n : "<<n<<'\n';
+    if(o>0) cout<<"o : "<<o<<'\n';
+    if(p>0) cout<<"p : "<<p<<'\n';
+    if(q>0) cout<<"q : "<<q<<'\n';
+    if(r>0) cout<<"r : "<<r<<'\n';
+    if(s>0) cout<<"s : "<<s<<'\n';
+    if(t>0) cout<<"t : "<<t<<'\n';
+    if(u>0) cout<<"u : "<<u<<'\n';
+    if(v>0) cout<<"v : "<<v<<'\n';
+    if(w>0) cout<<"w : "<<w<<'\n';
+    if(x>0) cout<<"x : "<<x<<'\n';
+    if(y>0) cout<<"y : "<<y<<'\n';
+    if(z>0) cout<<"z : "<<z<<'\n';
+    
+    return 0;
+}
