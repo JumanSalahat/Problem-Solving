e9531b7022d57f1924e636bbd891e50d7527fb64
JumanSalahat <juman.salahat@gmail.com>
2026-01-10 14:17:18 +0200

    Reverse String II
    Given a string s and an integer k, reverse the first k characters for every 2k characters counting from the start of the string.
    
    If there are fewer than k characters left, reverse all of them. If there are less than 2k but greater than or equal to k characters, then reverse the first k characters and leave the other as original.
    
    
    Example 1 :
    Input : s = "abcdefg", k = 2
    Output : "bacdfeg"
    
    Example 2 :
    Input : s = "abcd", k = 2
    Output : "bacd"
     
    
    Constraints :
    1 <= s.length <= 10^4
    s consists of only lowercase English letters.
    1 <= k <= 10^4

--- Leetcode/Difficulty : Easy/Topics : String/Reverse String II.cpp
+++ Leetcode/Difficulty : Easy/Topics : String/Reverse String II.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     string reverseStr(string s, int k) {
-        if(s.size()<=1) return s;
+        //if(s.size()<=1) return s;
+        // غير ضروري هالسطر إللي فوق ، لإنه تم حل هالحالة داخل الفور لوب -> if statement
     
         for(int i=0;i<s.size();i+=2*k)
         {
@@ -10,11 +11,10 @@ public:
              reverse(s.begin()+i, s.end());
              break; 
            }
-           
+
            reverse(s.begin()+i,s.begin()+i+k);
         }
         
-
         return s;
     }
 };
