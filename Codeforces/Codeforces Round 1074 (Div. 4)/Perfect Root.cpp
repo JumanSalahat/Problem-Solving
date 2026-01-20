4a13e7e523416784f2ebef71d7eef3aa88ad2862
JumanSalahat <juman.salahat@gmail.com>
2026-01-20 21:19:54 +0200

    Add Perfect Root program for Codeforces Round 1074
    A positive integer x is a perfect root if there exists an integer y such that sqrt(y)=x. For example, 5 is a perfect root because sqrt(25)=5.
    
    For each test case, output n distinct perfect roots. Note that the values only need to be distinct within each test case; you can use the same value in different test cases.
    
    Input
    The first line of the input contains a single integer t (1≤t≤20) — the number of test cases.
    
    The only line of each test case contains an integer n (1≤n≤20) — the number of perfect roots to output.
    
    Output
    For each test case, output n distinct perfect roots. Each perfect root x must be in the range 1≤x≤10^9.
    
    Example
    Input :
    3
    1
    2
    5
    
    Output :
    1
    2 4
    2 102 43 1 21
    
    Note
    For the first test case:
    1 is a perfect root because sqrt(1)=1.
    
    For the second test case:
    2 is a perfect root because sqrt(4)=2.
    4 is a perfect root because sqrt(16)=4 .

---
+++ Codeforces/Codeforces Round 1074 (Div. 4)/Perfect Root.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+// أيّ عدد موجب --> x  --> يُعتبر --> perfect root --> لإنّه بنقدر نختار --> y = x^2 --> وبالتالي --> sqrt(y) = x
+int main() {
+   int t; cin>>t;
+   while(t--)
+   {
+       int n;
+       cin>>n;
+       for(int i=1;i<=n;i++)
+       cout<<i<<" ";
+       
+       cout<<endl;
+   }
+}
