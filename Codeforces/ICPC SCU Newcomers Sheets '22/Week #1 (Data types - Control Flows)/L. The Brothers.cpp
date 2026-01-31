7cc039fc9ecde6cb96a3c02651ec0fc2ee2896f5
JumanSalahat <juman.salahat@gmail.com>
2026-01-31 06:41:45 +0200

    Create L. The Brothers.cpp
    Given two person names.
    
    Each person has {"the first name" + "the second name"}
    
    Determine whether they are brothers or not.
    
    Note: The two persons are brothers if they share the same second name.
    
    Input
    First line will contain two Strings F1, S1 which donates the first and second name of the 1st person.
    
    Second line will contain two Strings F2, S2 which donates the first and second name of the 2nd person.
    
    Output
    Print "ARE Brothers" if they are brothers otherwise print "NOT".
    
    
    Examples
    Input :
    bassam ramadan
    ahmed ramadan
    Output :
    ARE Brothers
    
    Input :
    ali salah
    ayman salah
    Output :
    ARE Brothers
    
    Input :
    ali kamel
    ali salah
    Output :
    NOT

---
+++ Codeforces/ICPC SCU Newcomers Sheets '22/Week #1 (Data types - Control Flows)/L. The Brothers.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s1,f1,s2,f2;
+    cin>>s1>>f1>>s2>>f2;
+    if(f1==f2) cout<<"ARE Brothers\n";
+    else cout<<"NOT\n";
+    return 0;
+}
