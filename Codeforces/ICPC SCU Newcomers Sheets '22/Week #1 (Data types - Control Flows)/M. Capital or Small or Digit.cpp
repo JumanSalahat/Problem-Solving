d9813090cb633dc09edd0ab9cb0021a62c79c73e
JumanSalahat <juman.salahat@gmail.com>
2026-02-01 14:39:42 +0200

    Create M. Capital or Small or Digit.cpp
    Given a letter X. Determine whether X is Digit or Alphabet and if it is Alphabet determine if it is Capital Case or Small Case.
    
    Note:
    Digits in ASCII '0' = 48,'1' = 49 ....etc
    Capital letters in ASCII 'A' = 65, 'B' = 66 ....etc
    Small letters in ASCII 'a' = 97,'b' = 98 ....etc
    Input
    Only one line containing a character X which will be a capital or small letter or digit.
    
    Output
    Print a single line contains "IS DIGIT" if X is digit otherwise, print "ALPHA" in the first line followed by a new line that contains "IS CAPITAL" if X is a capital letter and "IS SMALL" if X is a small letter.
    
    
    Examples
    Input :
    A
    Output :
    ALPHA
    IS CAPITAL
    
    Input :
    9
    Output :
    IS DIGIT
    
    Input :
    a
    Output :
    ALPHA
    IS SMALL
    
    
    Note
    ** recommended to read this to know more about ASCII Code https://www.javatpoint.com/ascii.

---
+++ Codeforces/ICPC SCU Newcomers Sheets '22/Week #1 (Data types - Control Flows)/M. Capital or Small or Digit.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char x;
+    cin>>x;
+    if(x>='a' && x<='z') cout<<"ALPHA\nIS SMALL\n";
+    else if(x>='A' && x<='Z') cout<<"ALPHA\nIS CAPITAL\n";
+    else if(x>='0' && x<='9') cout<<"IS DIGIT\n";
+    return 0;
+}
