9450ef4de440b7dfc3b680f5baa7af096e6c403c
JumanSalahat <juman.salahat@gmail.com>
2026-02-12 11:54:31 +0200

    Add Palindrome Array function implementation
    Given a number N and an array A of N numbers. Determine if it's palindrome or not.
    
    Note:
    An array is called palindrome if it reads the same backward and forward, for example, arrays { 1 } and { 1,2,3,2,1 } are palindromes, while arrays { 1,12 } and { 4,7,5,4 } are not.
    
    NOTE : Solve it using function.
    
    Input
    First line contains a number N ( 1 ≤ N ≤ 10^5 ) number of elements.
    
    Second line contains N numbers ( 1 ≤ Ai ≤ 10^9 ).
    
    Output
    Print "YES" (without quotes) if A is a palindrome array, otherwise, print "NO" (without quotes).
    
    
    Examples
    Input :
    5
    1 3 2 3 1
    Output :
    YES
    
    Input :
    4
    1 2 3 4
    Output :
    NO

---
+++ Codeforces/ICPC SCU Newcomers Sheets '22/Week #5 (Functions)/V. Palindrome Array.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool Palindrome_Array(int n,int a[])
+{
+   int x;
+   if(n%2==0) x=n/2;
+   else x=n/2 +1;
+   
+   for(int i=0;i<x;i++) // لو استخدمت -> n/2 -> مباشرة بدون -> x -> صح مية بالمية لو فردي أو زوجي 
+   // لإنه في حالة كانت -> n -> فردي.. آخر مقارنة رح تكون مقارنة العنصر الوسيط بنفسه 
+   {
+       if(a[i] != a[n-1-i]) return false; // a[n-1-i] لإنه بنبلش نقارن أول عنصر بآخر عنصر
+   }
+   
+   return true;
+}
+ 
+int main() {
+   int n; cin>>n;
+   int a[n];
+   for(int i=0;i<n;i++) cin>>a[i];
+   if(Palindrome_Array(n,a)== true) cout<<"YES\n";
+   else cout<<"NO\n";
+   return 0;
+}
