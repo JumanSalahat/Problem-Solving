2fddbe851b87613365609a11af24561ed92f97ff
JumanSalahat <juman.salahat@gmail.com>
2026-02-19 19:59:40 +0200

    Fix counting of consecutive characters in string
    Skibidus is given a string s that consists of lowercase Latin letters. If s contains more than 1 letter, he can:
    
    Choose an index i (1≤i≤|s|−1, |s| denotes the current length of s) such that si=si+1. 
    Replace si with any lowercase Latin letter of his choice. Remove si+1 from the string.
    Skibidus must determine the minimum possible length he can achieve through any number of operations.
    
    Input
    The first line contains an integer t (1≤t≤100) — the number of test cases.
    
    The only line of each test case contains a string s (1≤|s|≤100). It is guaranteed s only contains lowercase Latin letters.
    
    Output
    For each test case, output an integer on the new line, the minimum achievable length of s.
    
    Example
    Input :
    4
    baa
    skibidus
    cc
    ohio
    
    Output :
    1
    8
    1
    4
    
    
    Note
    In the first test case, Skibidus can:
    Perform an operation on i=2. He replaces s2 with b and removes s3 from the string. Then, s becomes bb.
    Perform an operation on i=1. He replaces s1 with b and removes s2 from the string. Then, s becomes b.
    Because s only contains 1 letter, Skibidus cannot perform any more operations.
    Therefore, the answer is 1 for the first test case.
    
    In the second test case, he cannot perform an operation on any index. Therefore, the answer is still the length of the initial string, 8.

--- Codeforces/GSG2025_Level01/GSG_Practice_202501-String/A. Skibidus and Ohio.cpp
+++ Codeforces/GSG2025_Level01/GSG_Practice_202501-String/A. Skibidus and Ohio.cpp
@@ -7,13 +7,12 @@ int main() {
     while(t--)
     {
         cin>>s;
-        int c=1;
-        int sum=0;
+        int c=0;
         for(int i=1;i<s.size();i++)
         {
             if(s[i]==s[i-1]) c++;
         }
-        if(c>1) cout<<1<<endl;
+        if(c>0) cout<<1<<endl;
         else cout<<s.size()<<endl;
     }    
     return 0;
