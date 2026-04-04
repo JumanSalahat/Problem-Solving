2d2b3506d80c374820a2f07646f314a7e236da0c
JumanSalahat <juman.salahat@gmail.com>
2026-04-04 20:06:43 +0300

    Add solution for The 67th Integer Problem
    https://codeforces.com/contest/2218/problem/A

---
+++ Codeforces/Codeforces Round 1090 (Div. 4)/A. The 67th Integer Problem.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   short t; cin>>t;
+   while(t--)
+   {
+       short x; cin>>x;
+       cout<<x<<endl; // or cout<<x+1; --> cout لعدد أكبر أو يساوي x
+   }
+}
