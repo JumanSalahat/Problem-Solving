4fab4ff2b5495e0056f8e086765f345e418b2334
JumanSalahat <juman.salahat@gmail.com>
2026-04-23 19:50:06 +0300

    Create A. Are You a Robot, Again?.cpp
    https://codeforces.com/contest/1952/problem/A

---
+++ Codeforces/April Fools Day Contest 2024/A. Are You a Robot, Again?.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    // If you select all the squares with traffic lights
+    // and read the corresponding letters from left-to-right, top-to-bottom,
+   // you get the text "print safety
+   // So all you need to do is print the word safety
+    
+    cout<<"safety\n";
+    return 0;
+}
