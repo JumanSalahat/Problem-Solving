318034ad7096d1bc389d10b44018de4c85f458ea
JumanSalahat <juman.salahat@gmail.com>
2026-04-25 10:39:10 +0300

    Add solution for 'Who Tested?' problem
    https://codeforces.com/contest/1663/problem/A

---
+++ Codeforces/April Fools Day Contest 2022/A. Who Tested?.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+// If you carefully read the announcement blog,
+// you will notice that I especially thanked BucketPotato for testing.
+// Just print "BucketPotato".
+
+
+int main() {
+   cout<<"BucketPotato"<<endl;
+   return 0;
+}
