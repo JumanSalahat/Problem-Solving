db8fdc2eb70ac895bdef9d1109e0dd2bc6fb5697
JumanSalahat <juman.salahat@gmail.com>
2026-04-28 22:21:43 +0300

    Add solution for 'Is it rated?' problem
    https://codeforces.com/contest/1331/problem/A

---
+++ Codeforces/April Fools Day Contest 2020/A. Is it rated?.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+// the contest announcement itself promised that the contest is not rated,
+//so the answer is a resolute ``NO'' (case insensitive, quotes for clarity only) :-)
+
+int main() {
+    cout<<"NO\n";
+    return 0;
+}
