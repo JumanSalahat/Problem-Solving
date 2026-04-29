eeb72d44328deaeaf82274eea316ddcc56d3347b
JumanSalahat <juman.salahat@gmail.com>
2026-04-29 11:17:14 +0300

    Add B. Limericks.cpp to find divisors
    https://codeforces.com/contest/1331/problem/B

---
+++ Codeforces/April Fools Day Contest 2020/B. Limericks.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+
+    for (int i = 2; i * i <= a; i++) {
+        if (a % i == 0) {
+            cout << i << a / i;
+            break;
+        }
+    }
+}
