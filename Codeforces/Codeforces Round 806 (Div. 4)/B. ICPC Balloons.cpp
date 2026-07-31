863240e277d613d3d8adc0b36711607586c1b569
JumanSalahat <juman.salahat@gmail.com>
2026-07-31 23:01:16 +0300

    Add solution for B. ICPC Balloons problem
    https://codeforces.com/contest/1703/problem/B

---
+++ Codeforces/Codeforces Round 806 (Div. 4)/B. ICPC Balloons.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int t; cin>>t;
+    while(t--)
+    {
+        int n; cin>>n;
+        string s; cin>>s;
+        sort(s.begin(),s.end());
+        int balloons=2;
+        for(int i=1;i<n;i++)
+        {
+            if(s[i]==s[i-1]) balloons++;
+            else balloons+=2;
+        }
+        cout<<balloons<<endl;
+    }
+    return 0;
+}
