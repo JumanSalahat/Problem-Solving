289e770e03dbfc2a45b47b572373b1f8e07c0959
JumanSalahat <juman.salahat@gmail.com>
2026-08-05 05:37:05 +0300

    Add solution for A. Riptide problem
    https://codeforces.com/contest/2254/problem/A

---
+++ Codeforces/Codeforces Round 1114 (Div. 3)/A. Riptide.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int t; cin>>t;
+    while(t--)
+    {
+        int a[3];
+        cin>>a[0]>>a[1]>>a[2];
+        int ans=0;
+        while(a[0] != a[1] && a[0] != a[2] && a[1] != a[2]) // مهم جداً ، انتباه للشرط
+        {
+            sort(a,a+3);
+            a[0]--;
+            a[2]++;
+            ans++;
+        }
+        cout<<ans<<endl;
+    }
+    return 0;
+}
