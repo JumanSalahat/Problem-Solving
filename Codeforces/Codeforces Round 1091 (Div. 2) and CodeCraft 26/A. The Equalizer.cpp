5de0ee0d88c3d57a0d90e29ea1ef0cb1ca59bc62
JumanSalahat <juman.salahat@gmail.com>
2026-08-07 08:18:28 +0300

    Add solution for 'The Equalizer' problem
    https://codeforces.com/problemset/problem/2217/A

---
+++ Codeforces/Codeforces Round 1091 (Div. 2) and CodeCraft 26/A. The Equalizer.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int t; cin>>t;
+    while(t--)
+    {
+        int n,k;
+        cin>>n>>k;
+        int a[n];
+        int sum=0;
+        for(int i=0;i<n;i++) 
+        { 
+            cin>>a[i];
+            sum+=a[i]; 
+        }
+
+        if( sum % 2 == 1 || (n*k) % 2 == 0 ) 
+        cout<<"YES\n";
+
+        else cout<<"NO\n";      
+
+    }
+    return 0;
+}
